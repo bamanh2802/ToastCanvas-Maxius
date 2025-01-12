fa7ed68b24165fc580b5a0d766f045f036ad367d
quycot <jizero2003@gmail.com>
2025-01-13 04:57:30 +0700

    'fi'

--- maxius/tailwind.config.js
+++ maxius/tailwind.config.js
@@ -1,5 +1,3 @@
-import {nextui} from '@nextui-org/theme'
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -19,5 +17,5 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 }
